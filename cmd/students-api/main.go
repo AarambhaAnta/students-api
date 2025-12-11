28e53f6225805f6092862a85299b5443afed10e3
AarambhaAnta <aarambha108@gmail.com>
2025-12-12 00:54:50 +0530

    feat: create student endpoint

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/AarambhaAnta/students-api/internal/config"
 	"github.com/AarambhaAnta/students-api/internal/http/handlers/student"
+	"github.com/AarambhaAnta/students-api/internal/storage/sqlite"
 )
 
 func main() {
@@ -20,10 +21,17 @@ func main() {
 
 	// logger <- will use built-in
 	// database setup
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
 	// setup server
 	server := http.Server{
@@ -53,7 +61,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 	if err != nil {
 		slog.Error("failed to shut-down server", slog.String("error", err.Error()))
 	}
