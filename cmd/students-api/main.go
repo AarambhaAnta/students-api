62112090d07538cc4b79893e060963a094eabe50
AarambhaAnta <aarambha108@gmail.com>
2025-12-11 22:45:40 +0530

    feat: basic server setup

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/AarambhaAnta/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students api")
+	// load config
+	cfg := config.MustLoad()
+
+	// logger <- will use built-in
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students API."))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("server started... %s", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server.")
+	}
+
 }
