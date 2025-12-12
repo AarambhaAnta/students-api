df60eb58edc74500e236ac656cdc8759727e985a
AarambhaAnta <aarambha108@gmail.com>
2025-12-12 11:51:19 +0530

    feat: add student by id

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/AarambhaAnta/students-api/internal/storage"
 	"github.com/AarambhaAnta/students-api/internal/types"
@@ -54,5 +55,28 @@ func New(storage storage.Storage) http.HandlerFunc {
 		}
 
 		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
+	}
+}
+
+func GetByID(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			slog.Error("error parsing id to int64", slog.String("id", id))
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		student, err := storage.GetStudentByID(intId)
+
+		if err != nil {
+			slog.Error("error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
 	}
 }
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,8 +2,11 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
+	"log/slog"
 
 	"github.com/AarambhaAnta/students-api/internal/config"
+	"github.com/AarambhaAnta/students-api/internal/types"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -51,4 +54,28 @@ func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error
 	}
 
 	return lastId, nil
+}
+
+func (s *Sqlite) GetStudentByID(id int64) (types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students WHERE id = ? LIMIT 1")
+	if err != nil {
+		slog.Error("error preparing SQL query")
+		return types.Student{}, err
+	}
+	defer stmt.Close()
+
+	var student types.Student
+
+	err = stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+	if err != nil {
+		slog.Error("error getting data of", "id", id)
+		if err == sql.ErrNoRows {
+			slog.Error("no student with ", "id", id)
+			return types.Student{}, fmt.Errorf("no student found with id %s", fmt.Sprint(id))
+		}
+
+		return types.Student{}, fmt.Errorf("query error: %w", err)
+	}
+
+	return student, nil
 }
